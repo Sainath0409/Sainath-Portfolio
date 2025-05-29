c967768e4967474ec3d93686835217f9893bc38c
A Sainath Patro <120198391+Sainath0409@users.noreply.github.com>
2025-05-29 15:08:17 +0530

    src file added

---
+++ src/components/Header.jsx
@@ -0,0 +1,138 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-scroll";
+import { changeTheme } from "../utils/themeSlice";
+
+export const Header = () => {
+  const theme = useSelector((store) => store?.theme?.theme);
+  const dispatch = useDispatch();
+  const navs = ["home", "about", "skills", "projects", "experience"];
+  const [navCollapse, setNavCollapse] = useState(true);
+  const changeMode = () => {
+    if (theme === "light") {
+      dispatch(changeTheme("dark"));
+    } else {
+      dispatch(changeTheme("light"));
+    }
+  };
+  return (
+    <header className="flex md:px-28 bg-white dark:bg-black dark:bg-opacity-80 justify-between shadow-md p-3 items-center fixed backdrop-filter backdrop-blur-md  top-0 z-40 w-full">
+      <div className="">
+        <h1 className="text-xl md:text-3xl font-bold relative dark:text-white">
+          A Sainath <span className="text-[#DC143C]">Patro</span>
+        </h1>
+      </div>
+      <div className="flex gap-3 md:gap-10">
+        <div className="md:flex gap-10 hidden">
+          <Link
+            className="font-medium text-gray-600 hover:text-[#DC143C] cursor-pointer dark:text-white"
+            to={"home"}
+            offset={-60}
+            smooth={true}
+            duration={500}
+            isDynamic={true}
+          >
+            Home
+          </Link>
+          <Link
+            className="font-medium text-gray-600 hover:text-[#DC143C] cursor-pointer dark:text-white"
+            to={"about"}
+            offset={-60}
+            smooth={true}
+            duration={500}
+            isDynamic={true}
+          >
+            About
+          </Link>
+
+          <Link
+            className="font-medium text-gray-600 hover:text-[#DC143C] cursor-pointer dark:text-white"
+            to={"skills"}
+            offset={-60}
+            smooth={true}
+            duration={500}
+            isDynamic={true}
+          >
+            Skills
+          </Link>
+          <Link
+            className="font-medium text-gray-600 hover:text-[#DC143C] cursor-pointer dark:text-white"
+            to={"projects"}
+            offset={-60}
+            smooth={true}
+            duration={500}
+            isDynamic={true}
+          >
+            Projects
+          </Link>
+          <Link
+            className="font-medium text-gray-600 hover:text-[#DC143C] cursor-pointer dark:text-white"
+            to={"contact"}
+            offset={-60}
+            smooth={true}
+            duration={500}
+            isDynamic={true}
+          >
+            Contact
+          </Link>
+        </div>
+        <button
+          onClick={changeMode}
+          className="rounded-full h-7 w-7 p-2 md:hover:bg-gray-200 bg-gray-200 flex justify-center items-center dark:bg-[#DC143C]"
+        >
+          <span
+            class={
+              "material-symbols-outlined text-xl text-gray-600 dark:text-white hover:text-black"
+            }
+          >
+            {theme === "light" ? "dark_mode" : "light_mode"}
+          </span>
+        </button>
+        <button
+          onClick={() => setNavCollapse(false)}
+          className="rounded-full h-7 w-7 hover:bg-gray-200 md:hidden dark:text-white"
+        >
+          <span class="material-symbols-outlined text-2xl">segment</span>
+        </button>
+        <div
+          className={`flex min-h-screen w-screen absolute md:hidden top-0 ${
+            !navCollapse ? "right-0" : "right-[-100%]"
+          } bottom-0 z-50 ease-in duration-300`}
+        >
+          <div className="w-1/4" onClick={() => setNavCollapse(true)}></div>
+
+          <div className="flex flex-col p-4 gap-5 bg-gray-100/95 backdrop-filter backdrop-blur-sm dark:bg-grey-900/95 w-3/4">
+            <button
+              onClick={() => setNavCollapse(true)}
+              class="bi bi-x text-xl self-end my-2 rounded-full h-7 w-7 bg-gray-200 flex items-center justify-center hover:bg-[#DC143C] hover:text-white"
+            ></button>
+            {navs.map((e) => (
+              <Link
+                key={e}
+                className="dark:text-white hover:text-[#DC143C] py-1.5 px-4 rounded transition-colors capitalize cursor-pointer"
+                to={e}
+                offset={-60}
+                smooth={true}
+                duration={500}
+                isDynamic={true}
+                onClick={() => setNavCollapse(true)}
+              >
+                {e}
+              </Link>
+            ))}
+            <Link
+              to="contact"
+              offset={-60}
+              smooth={true}
+              duration={500}
+              onClick={() => setNavCollapse(true)}
+              className="dark:text-white hover:text-[#DC143C] py-1.5 px-4 rounded transition-colors capitalize cursor-pointer"
+            >
+              Contact
+            </Link>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
---
+++ src/data.js
@@ -0,0 +1,319 @@
+export const data = {
+  main: {
+    name: "Sainath",
+    shortDesc:
+      "Explore my work in front-end development, where I leverage React.js and Next.js to craft user-friendly web applications. My focus is consistently on understanding and delivering Clientrequirements, ensuring robust scalability and exceptional performance through meticulous attention to detail.",
+    titles: ["Front End Development", "Web Developer", "React js Developer"],
+    heroImage:
+      "https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg",
+    techStackImages: [
+      "https://img.icons8.com/color/144/null/javascript--v1.png",
+      "https://img.icons8.com/color/144/null/tailwindcss.png",
+      "https://img.icons8.com/fluency/144/null/nextjs.png",
+      "https://img.icons8.com/ultraviolet/120/null/react--v1.png",
+    ],
+  },
+  about: {
+    aboutImage:
+      "https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg",
+    aboutImageCaption: "<Code. Create. Deploy. />",
+    title:
+      "Front-End Developer | Proficient in React.js, Next.js & Tailwind CSS | Experienced in Responsive Design, API Integration & Scalable UI Components | Dedicated to Building Seamless, User-Centric Web Experiences",
+    about:
+      "I’m A. Sainath Patro, a Front-End Developer skilled in React.js, Next.js, and Tailwind CSS. I specialize in building responsive and scalable web applications using clean, efficient code. I’ve contributed to projects like healthcare systems, invoice and inventory dashboards, and vendor management tools—focusing on performance, usability, and seamless API integration. I’m passionate about turning requirements into functional, real-world web solutions and enjoy working in collaborative, fast-paced environments. I’m always eager to learn new technologies and build web applications that make a meaningful impact. Let’s connect — I’m always excited to collaborate and build impactful web solutions.",
+    callUrl: "",
+    resumeUrl: "",
+  },
+  socials: [
+    {
+      icon: "bi bi-linkedin",
+      link: "https://www.linkedin.com/in/a-sainath-patro-7a844b215/",
+    },
+    {
+      icon: "bi bi-github",
+      link: "https://github.com/sainath0409",
+    },
+    {
+      icon: "bi bi-instagram",
+      link: "https://www.instagram.com/rock___sai/?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D",
+    },
+    {
+      icon: "bi bi-facebook",
+      link: "https://www.facebook.com/rock.sai.16568",
+    },
+  ],
+  skills: [
+    {
+      name: "HTML 5",
+      image: "https://img.icons8.com/color/144/null/html-5--v1.png",
+      category: "Frontend",
+    },
+    {
+      name: "CSS 3",
+      image: "https://img.icons8.com/color/144/null/css3.png",
+      category: "Frontend",
+    },
+    {
+      name: "JavaScript",
+      image: "https://img.icons8.com/color/144/null/javascript--v1.png",
+      category: "Frontend",
+    },
+    {
+      name: "React JS",
+      image: "https://img.icons8.com/ultraviolet/120/null/react--v1.png",
+      category: "Frontend",
+    },
+    {
+      name: "Next.js",
+      image: "https://img.icons8.com/fluency/144/null/nextjs.png",
+      category: "Frontend",
+    },
+    {
+      name: "Bootstrap",
+      image: "https://img.icons8.com/color/144/null/bootstrap.png",
+      category: "Frontend",
+    },
+    {
+      name: "Tailwind",
+      image: "https://img.icons8.com/color/144/null/tailwindcss.png",
+      category: "Frontend",
+    },
+    {
+      name: "Material UI",
+      image: "https://img.icons8.com/color/144/null/material-ui.png",
+      category: "Frontend",
+    },
+    {
+      name: "TypeScript",
+      image: "https://img.icons8.com/color/144/null/typescript.png",
+      category: "Frontend",
+    },
+    {
+      name: "Vite",
+      image: "https://vitejs.dev/logo-with-shadow.png",
+      category: "Frontend",
+    },
+    {
+      name: "Sass",
+      image: "https://img.icons8.com/color/144/null/sass.png",
+      category: "Frontend",
+    },
+    {
+      name: "GraphQL",
+      image: "https://img.icons8.com/color/144/null/graphql.png",
+      category: "Frontend",
+    },
+    {
+      name: "Git",
+      image: "https://img.icons8.com/color/144/null/git.png",
+      category: "Tools",
+    },
+    {
+      name: "GitHub",
+      image:
+        "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      category: "Tools",
+    },
+    {
+      name: "Figma",
+      image: "https://img.icons8.com/color/144/null/figma.png",
+      category: "Tools",
+    },
+    {
+      name: "VS Code",
+      image: "https://img.icons8.com/fluent/144/visual-studio-code-2019.png",
+      category: "Tools",
+    },
+    {
+      name: "Postman",
+      image: "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg",
+      category: "Tools",
+    },
+    {
+      name: "Webpack",
+      image: "https://img.icons8.com/color/144/null/webpack.png",
+      category: "Tools",
+    },
+    {
+      name: "Docker",
+      image: "https://img.icons8.com/color/144/null/docker.png",
+      category: "Tools",
+    },
+    {
+      name: "Axios",
+      image: "https://avatars.githubusercontent.com/u/32372333?s=200&v=4",
+      category: "Tools",
+    },
+  ],
+  projects: [
+    {
+      name: "Hospital Revenue Cycle Management (RCM) System",
+      techstack: "React JS, JavaScript, HtML 5, CSS 3, Botstrap",
+      details:
+        "The solution significantly improved the discharge experience for patients and their families while increasing operational efficiency for the hospital.",
+      category: "Client",
+      image: "/img/RCM_p1.jpg",
+      links: {
+        code: "",
+        video: "",
+        visit:
+          "https://www.techtarget.com/searchhealthit/definition/revenue-cycle-management-RCM",
+      },
+    },
+    {
+      name: "Intelligent Invoice Management System (IIMS)",
+      techstack:
+        "React JS, JavaScript, HtML 5, CSS 3, Botstrap,Tailwind CSS, Material UI,Shadcn UI",
+      details:
+        "The Intelligent Invoice Management System (IIMS) is a comprehensive solution designed to streamline the invoice management process for businesses. It automates the creation, tracking, and management of invoices, reducing manual errors and improving efficiency.",
+      category: "Client",
+      image: "/img/IIMS_p2.png",
+      links: {
+        code: "",
+        video: "",
+        visit: "https://www.stampli.com/invoice-management/",
+      },
+    },
+    {
+      name: "Vendor Management System (VMS)",
+      techstack:
+        "Vite React JS, JavaScript, HtML 5, CSS 3, Botstrap,Tailwind CSS, Material UI,Shadcn UI",
+      details:
+        "The Vendor Management System (VMS) is a comprehensive platform designed to streamline the management of vendor relationships and procurement processes. It automates vendor onboarding, contract management, performance tracking, and compliance monitoring.",
+      category: "Client",
+      image: "/img/VMS_p3.png",
+      links: {
+        code: "",
+        video: "",
+        visit:
+          "https://kissflow.com/procurement/vendor-management/guide-to-vendor-management-system/",
+      },
+    },
+    {
+      name: "Inventory Management System (IMS)",
+      techstack:
+        "Vite React JS, JavaScript, HtML 5, CSS 3, Botstrap,Tailwind CSS,Shadcn UI",
+      details:
+        "The Inventory Management System (IMS) is a robust solution designed to optimize inventory control and streamline supply chain operations. It automates inventory tracking, order management, and stock replenishment, ensuring accurate real-time visibility of inventory levels.",
+      category: "Client",
+      image: "/img/IMS_p4.png",
+      links: {
+        code: "",
+        video: "",
+        visit: "https://jelvix.com/blog/automated-inventory-management-system",
+      },
+    },
+    {
+      name: "Integrated Billing & Table Booking System with Online Menu for Restaurants and Cafés",
+      techstack:
+        "Vite React JS, JavaScript, HtML 5, CSS 3,Next js,Tailwind CSS,Shadcn UI,typescript",
+      details:
+        "The Integrated Billing & Table Booking System is a comprehensive solution designed for restaurants and cafés to streamline their operations. It combines online menu management, table booking, and billing functionalities into a single platform, enhancing customer experience and operational efficiency.",
+      category: "Client",
+      image: "/img/Billing_p5.png",
+      links: {
+        code: "",
+        video: "",
+        visit: "https://www.opentable.com/restaurant-solutions/",
+      },
+    },
+    {
+      name: "Role-Based E-Commerce Platform with Digital Product Support",
+      techstack:
+        "Vite React JS, JavaScript, HtML 5, CSS 3,Next js,Tailwind CSS,Shadcn UI,typescript",
+      details:
+        "The Role-Based E-Commerce Platform is a versatile solution designed to cater to various user roles, including customers, vendors, and administrators. It supports digital products, allowing for seamless transactions and user management, the platform provides a complete and scalable online shopping experience.",
+      category: "Client",
+      image: "/img/ecomm_duplicate.jpg",
+      links: {
+        code: "",
+        video: "",
+        visit: "https://www.cloudways.com/blog/ecommerce-best-practices/",
+      },
+    },
+    {
+      name: "React-Js-CRUD-App",
+      techstack: "React JS, JavaScript, HtML 5, CSS 3,Bootstrap",
+      details:
+        "This is a simple React JS CRUD application that allows users to create, read, update, and delete data. It uses local storage to persist data across sessions.",
+      category: "Personal",
+      image: "/img/CRUD_c1.png",
+      links: {
+        code: "https://github.com/Sainath0409/React-Js-CRUD-",
+        video: "",
+        visit:
+          "https://www.geeksforgeeks.org/how-to-do-crud-operations-in-reactjs/",
+      },
+    },
+     {
+      name: "Learning-Management-System-(LMS)",
+      techstack:
+        "React JS, JavaScript, HtML 5, CSS 3,Bootstrap,Node JS,Express JS,MongoDB",
+      details:
+        "This is a Learning Management System (LMS) built using React JS, Node JS, Express JS, and MongoDB. It allows users to create, manage, and track online courses and learning materials.",
+      category: "Personal",
+      image: "/img/LMS_c2.png",
+      links: {
+        code: "https://github.com/Sainath0409/Learning-Management-System-LMS-",
+        video: "",
+        visit:
+          "https://mintbook.com/blog/what-is-a-learning-management-system-lms/",
+      },
+    },
+    {
+      name: "Digital calclulator",
+      techstack: "HtML 5, CSS 3,JavaScript",
+      details:
+        "This is a simple digital calculator built using React JS. It allows users to perform basic arithmetic operations like addition, subtraction, multiplication, and division.",
+      category: "Personal",
+      image: "/img/calculator-c3.png",
+      links: {
+        code: "https://github.com/Sainath0409/CODSOFT-Lavel-01/tree/main/CALCULATOR",
+        video: "",
+        visit:
+          "https://electronics.howstuffworks.com/gadgets/other-gadgets/calculator.htm",
+      },
+    },
+  ],
+  educations: [
+    {
+      institute: "Indira Gandhi Institute Of Technology, Sarang",
+      degree: "B.Tech in Electrical Engineering",
+      duration: "2018 - 2022",
+    },
+    {
+      institute: "Arihant Junior College, Berhampur",
+      degree: "CHSE Science",
+      duration: "2016 - 2018",
+    },
+    {
+      institute: "Maa Saraswati vidya Mandir, Berhampur",
+      degree: "BSE ",
+      duration: "2015 - 2016",
+    },
+  ],
+  experiences: [
+    {
+      company: "Netlabs Global IT Services Pvt. Ltd.",
+      position: "Junior Software Engineer",
+      duration: "August 2023 - December 2024",
+      desc: [
+        "Developed user-friendly front-end interfaces for Hospital Equipment Revenue Cycle Management, improving equipment tracking and patient safety.",
+        "Designed interactive dashboards for Invoice Management and Inventory Systems, enabling real-time financial analytics and stock visibility.",
+        "Implemented reporting tools in Vendor Management System to monitor KPIs, optimize vendor relations, and reduce risks.",
+        "Built multiple pages and features from scratch using HTML, CSS, JavaScript, TailwindCSS, ReactJS, NodeJS, MongoDB, and REST APIs.",
+      ],
+    },
+    {
+      company: "The Marble Jar soluctions Pvt. Ltd.",
+      position: "Front-end Developer",
+      duration: "January 2025 - Present",
+      desc: [
+        "Developed responsive food ordering and table booking platforms for cafés and restaurants, ensuring a seamless experience across mobile, tablet, laptop, and smart TV screens.",
+        "Built a fully responsive e-commerce website, optimizing UI/UX for improved engagement and conversion across all devices.",
+        "Collaborated closely on building projects from the ground up, contributing to both UI design and functional implementation.",
+        "Tech stack: HTML, CSS, JavaScript, TailwindCSS, ReactJS, NodeJS, MongoDB, REST APIs.",
+      ],
+    },
+  ],
+};
